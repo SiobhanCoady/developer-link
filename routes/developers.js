8d5a9f7b3e92b0f081deebb885b44b9912641ec8
Siobhan Coady <siobhan.coady@gmail.com>
2017-05-09 21:38:37 -0700

    Display developers on developers browse page

--- routes/developers.js
+++ routes/developers.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/index').User;
 
 router.get('/', function(req, res, next) {
-  res.render('developers/index', { title: 'Web Dev Link' });
+  User
+    .findAll({
+      where: {userType: 'developer'},
+      order: [['createdAt', 'DESC']]})
+    .then(function(developers) {
+      res.render('developers/index', {title: 'Web Dev Link',
+                                      developers: developers
+                                      });
+    });
+  // res.render('developers/index', { title: 'Web Dev Link' });
 });
 
 module.exports = router;
