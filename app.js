c762c1bf2ff1358c8ecdccd85d5e101ab7528913
Siobhan Coady <siobhan.coady@gmail.com>
2017-05-12 17:30:26 -0700

    Add passport, passport-local, express-session, and connect-flash to project

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+var expressSession = require('express-session');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,11 +22,24 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(expressSession({secret: 'siobhansecret',
+                        resave: false,
+                        saveUninitialized: false,
+                        cookie: { maxAge: 6000000 }}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.success   = req.flash('success').join(', ');
+  res.locals.error     = req.flash('error').join(', ');
+  res.locals.info      = req.flash('info').join(', ');
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
